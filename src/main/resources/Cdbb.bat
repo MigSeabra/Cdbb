682ea88377768cfee46354f06ed37c3d049ff90c
Miguel Seabra <miguel.seabra@sapo.pt>
2019-05-19 20:22:52 +0100

    Jira ID Changed batch file for -h support

--- src/main/resources/Cdbb.bat
+++ src/main/resources/Cdbb.bat
@@ -4,5 +4,9 @@ for %%x in (%*) do Set /A argC+=1
 if /I "%argC%" NEQ "1" (
 	echo USAGE: .\Cdbb.bat [file path]
 ) else (
-	java -jar Cdbb-0.1-jar-with-dependencies.jar %1
+	if /I "%1" == "-h" (
+		echo USAGE: .\Cdbb.bat [file path]
+	) else (
+		java -jar Cdbb-0.1-jar-with-dependencies.jar %1
+	)
 )
